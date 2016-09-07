cd5444defb577b80007304549cc806dd684f5e4c
sdc <heysdc@163.com>
2016-09-08 07:27:47 +0800

    redux

--- client/index.js
+++ client/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { immutableRenderDecorator } from 'react-immutable-render-mixin'
 import { fromJS } from 'immutable'
+import { createStore } from 'redux'
+import reducers from './components/public/reducers'
+import * as action from './components/public/action'
 
 @immutableRenderDecorator
 class World extends React.Component {
@@ -10,9 +13,23 @@ class World extends React.Component {
       title: 'Hello, world!'
     })
   }
+
+  store = createStore(reducers)
+
+  unSubscribeAdd = this.store.subscribe(() => {
+    console.log(this.store.getState())
+  })
+
+  add () {
+    this.store.dispatch(action.addTodo('sb'))
+  }
+
   render () {
-    console.log('ss', this.state.data.get('title'))
-    return <h1>Hello, world!sb</h1>
+    return <div>
+      <input type='text' />
+      <button onClick={this.add.bind(this)}>确认</button>
+      <button onClick={this.unSubscribeAdd}>remove</button>
+    </div>
   }
 }
 
