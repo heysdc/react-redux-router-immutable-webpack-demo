e952b9e7ed03bb23bed849c5e85e88fcd70172c3
sdc <heysdc@163.com>
2016-09-20 11:56:33 +0800

    require

--- client/initialStore.js
+++ client/initialStore.js
@@ -1,42 +1,42 @@
 import * as reducers from './reducers'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
 import initialState from './initialState'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 import { hashHistory } from 'react-router'
 import Immutable from 'immutable'
 
-const logger = createLogger({
-  stateTransformer: (state) => {
-    let newState = {}
+const middleWare = [thunkMiddleware]
 
-    for (var i of Object.keys(state)) {
-      if (Immutable.Iterable.isIterable(state[i])) {
-        newState[i] = state[i].toJS()
-      } else {
-        newState[i] = state[i]
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger')
+  const logger = createLogger({
+    stateTransformer: (state) => {
+      let newState = {}
+
+      for (var i of Object.keys(state)) {
+        if (Immutable.Iterable.isIterable(state[i])) {
+          newState[i] = state[i].toJS()
+        } else {
+          newState[i] = state[i]
+        }
       }
+      return newState
     }
-    console.log('newState', newState, newState)
-
-    return newState
-  }
-})
+  })
+  middleWare.push(logger)
+}
 
 const rootReducer = combineReducers({
   ...reducers,
   routing: routerReducer
 })
 
-const loggerMiddleware = logger
-
 const store = createStore(
   rootReducer,
   initialState,
   applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
+    ...middleWare
   )
 )
 
